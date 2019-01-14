02844f1634a0262d39067979391528847fdd5007
Iulian <hojdaiulian39@gmail.com>
2019-01-14 21:05:38 +0200

    some random code

---
+++ distributed/script_StrongScaling.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -d StrongScaling_CSV_N2_Solution ]
+        then
+        rm -rf StrongScaling_CSV_N2_Solution
+        mkdir StrongScaling_CSV_N2_Solution
+        else
+        mkdir StrongScaling_CSV_N2_Solution
+fi
+
+/oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpicc $1 -o $1.out -lm
+
+for((Y=50000; Y<=150000; Y+=50000))
+do
+        echo "particles,rank,time" >> stderrorStrongScaling_$Y.csv
+
+        for((X=2;X<=16; X=X*2))
+        do
+        
+                if [ -f generate_output.txt ]
+                        then
+                        rm generate_output.txt
+                fi
+        
+        /oxygen/compilers/mpi/openmpi/1.8.2.jdk.1.8/bin/mpirun -np $X -host oxygen9,oxygen10,oxygen11,oxygen12,oxygen13,oxygen14,oxygen15,oxygen16,oxygen17,oxygen19,oxygen20,oxygen21,oxygen22,oxygen23,oxygen24 $1.out $Y 10 0.1 1 g 2>> stderrorStrongScaling_$Y.csv 
+        
+        done
+
+        mv stderrorStrongScaling_$Y.csv StrongScaling_CSV_N2_Solution/
+done
+
+rm $1.out
+
+exit 0
---
+++ parallel/parallel.h
@@ -0,0 +1,23 @@
+#ifndef PARALLEL_H
+#define PARALLEL_H
+
+#include <pthread.h>
+
+void ComputeForces();
+void* ComputeForcesThread(void*);
+void ComputeNewPos();
+void* ComputeNewPosThread(void*);
+void InitParticles();
+
+typedef struct {
+	double x, y, z;
+	double mass;
+} Particle;
+
+typedef struct {
+	double xold, yold, zold;
+	double fx, fy, fz;
+} ParticleV;
+
+#endif
+
